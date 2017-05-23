bc76524b525ba4d732e9411365b915723910f6ac
Pierre Bouvier <Pierre.Bouvier1@ensimag.grenoble-inp.fr>
2017-05-23 15:35:49 +0200

    Clipper alpha edition

--- src/ei_draw.c
+++ src/ei_draw.c
@@ -21,19 +21,25 @@ void ei_draw_polyline (ei_surface_t			surface,
 						 const ei_color_t		color,
 						 const ei_rect_t*		clipper) {
     if(first_point == NULL) return;
-	uint32_t *buff = (uint32_t *) hw_surface_get_buffer(surface);
-	int W = hw_surface_get_size(surface).width;
+	// uint32_t *buff = (uint32_t *) hw_surface_get_buffer(surface);
+	// int W = hw_surface_get_size(surface).width;
 	uint32_t col = ei_map_rgba(surface, &color);
 	ei_point_t pt_min = first_point->point;
 	ei_point_t pt_max = first_point->point;
 	ei_linked_point_t *ptr_tmp = (ei_linked_point_t *) first_point;
 	while(ptr_tmp != NULL) {
-		pt_min.x = (pt_min.x > ptr_tmp->point.x) ? pt_min.x : ptr_tmp->point.x;
-		pt_min.y = (pt_min.y > ptr_tmp->point.y) ? pt_min.y : ptr_tmp->point.y;
+		pt_min.x = (pt_min.x < ptr_tmp->point.x) ? pt_min.x : ptr_tmp->point.x;
+		pt_min.y = (pt_min.y < ptr_tmp->point.y) ? pt_min.y : ptr_tmp->point.y;
 		pt_max.x = (pt_max.x > ptr_tmp->point.x) ? pt_max.x : ptr_tmp->point.x;
 		pt_max.y = (pt_max.y > ptr_tmp->point.y) ? pt_max.y : ptr_tmp->point.y;
 		ptr_tmp = ptr_tmp->next;
 	}
+	ei_size_t box = {pt_max.x - pt_min.x + 1, pt_max.y - pt_min.y + 1};
+	ei_surface_t n_surface = hw_surface_create(surface, &box, 0);
+	hw_surface_lock(n_surface);
+	uint32_t *n_buff = (uint32_t *) hw_surface_get_buffer(n_surface);
+	int offset = pt_min.x + pt_min.y * box.width;
+
 	ptr_tmp = (ei_linked_point_t *) first_point;
 	while(ptr_tmp != NULL) {
 		ei_point_t pt_0 = ptr_tmp->point;
@@ -45,7 +51,7 @@ void ei_draw_polyline (ei_surface_t			surface,
 		int incr_y = 1 - 2 * (pt_0.y >= pt_1.y);
 		int E = dx - dy;
 		do {
-			buff[pt_0.x + W * pt_0.y] = col; //TODO clipper
+			n_buff[pt_0.x + box.width * pt_0.y - offset] = col;
 			if(2 * E > - dy) {
 				E -= dy;
 				pt_0.x += incr_x;
@@ -57,10 +63,10 @@ void ei_draw_polyline (ei_surface_t			surface,
 		} while(pt_0.x != pt_1.x || pt_0.y != pt_1.y);
 		ptr_tmp = ptr_tmp->next;
 	}
-	ei_size_t update_size = {pt_max.x - pt_min.x, pt_max.y - pt_min.y};
-	ei_rect_t rect = {pt_min, update_size};
-	ei_linked_rect_t linked_rect = {rect, NULL};
-	hw_surface_update_rects(surface, &linked_rect);
+	ei_rect_t dst_rect = {pt_min, box};
+	printf("%d", ei_copy_surface(surface, &dst_rect, n_surface, NULL, 0));
+	hw_surface_unlock(n_surface);
+	hw_surface_free(n_surface);
 }
 
 struct polygon_side{
@@ -334,9 +340,6 @@ int	ei_copy_surface(ei_surface_t destination,
  	ei_size_t src_size = hw_surface_get_size(source);
 	uint32_t *dst_buff = (uint32_t *) hw_surface_get_buffer(destination);
 	ei_size_t dst_size = hw_surface_get_size(destination);
-
-	printf("src (cx,cy) : %d %d\n", src_rect->top_left.x, src_rect->top_left.y);
-	printf("dst (cx,cy) : %d %d\n", dst_rect->top_left.x, dst_rect->top_left.y);
 
 	ei_point_t src_clipper_pt;
 	ei_size_t src_clipper_size;
