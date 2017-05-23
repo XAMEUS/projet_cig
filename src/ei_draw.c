85e4a7b20f674906eeb79d8548666aa7085270af
Pierre Bouvier <Pierre.Bouvier1@ensimag.grenoble-inp.fr>
2017-05-23 09:50:00 +0200

    polyline: Ok, but need clipper

--- src/ei_draw.c
+++ src/ei_draw.c
@@ -1,12 +1,6 @@
 #include "ei_draw.h"
 #include <stdlib.h>
 
-void private_ei_draw_line (ei_surface_t			surface,
-						 ei_point_t           	a,
-                         ei_point_t             b,
-						 const ei_color_t		color,
-						 const ei_rect_t*		clipper);
-
 uint32_t ei_map_rgba(ei_surface_t surface, const ei_color_t* color) {
     int r, g, b, a;
     hw_surface_get_channel_indices(surface, &r, &g, &b, &a);
@@ -17,92 +11,62 @@ uint32_t ei_map_rgba(ei_surface_t surface, const ei_color_t* color) {
                     ((a >= 0) ? (color->alpha << (8*(3-a))) : 0);
     return c;
 }
-void private_ei_draw_line (ei_surface_t			surface,
-						 ei_point_t           	pt_0,
-                         ei_point_t             pt_1,
-						 const ei_color_t		color,
-						 const ei_rect_t*		clipper) {
-	int dx = abs(pt_1.x - pt_0.x);
-    int dy = abs(pt_1.y - pt_0.y);
-	int incr_x = 1 - 2 * (pt_0.x >= pt_1.x);
-    int incr_y = 1 - 2 * (pt_0.y >= pt_1.y);
-	int E = dx - dy;
-	//Dessiner(x, y)
-	while(pt_0.x != pt_1.x || pt_0.y != pt_1.y) {
-		printf("=>%d %d %d %d\n", pt_0.x, pt_0.y, pt_1.y, pt_1.y);
-		//Dessiner(x, y)
-		if(2 * E > - dy) {
-			E -= dy;
-			pt_0.x += incr_x;
-		}
-		// if(pt_0.x == pt_1.x && pt_0.y == pt_1.y) break;
-		if(2 * E < dx) {
-			E += dx;
-			pt_0.y += incr_y;
-		}
-	}
-// 	int abs_dx = abs(b.x - a.x);
-//     int abs_dy = abs(b.y - a.y);
-//     int incr_x = 1 - 2 * (b.x < a.x);
-//     int incr_y = 1 - 2 * ((b.y > a.y) ^ (abs_dx < abs_dy));
-//     if(abs_dx > abs_dy) {
-//         int x = a.x;
-//         int y = a.y;
-//         int E = 0;
-//         while(x != b.x) {
-//             printf("");
-//             //Dessiner(x,y)
-//             x += incr_x;
-//             E += abs_dy;
-//             if(2 * E > abs_dx) {
-//                 y += incr_y;
-//                 E -= abs_dx;
-//             }
-//         }
-//     }
-//     else {
-//         int y = a.y;
-//         int x = a.x;
-//         int E = 0;
-//         while(y != b.y) {
-//             printf("%d, %d\n", abs_dx, abs_dy);
-//             //Dessiner(x,y)
-//             y += incr_y;
-//             E += abs_dx;
-//             if(2 * E > abs_dy) {
-//                 x += incr_x;
-//                 E -= abs_dy;
-//             }
-//         }
-//     }
-}
 
-/**
- * \brief	Draws a line made of many line segments.
- *
- * @param	surface 	Where to draw the line. The surface must be *locked* by
- *				\ref hw_surface_lock.
- * @param	first_point 	The head of a linked list of the points of the line. It can be NULL
- *				(i.e. draws nothing), can have a single point, or more.
- *				If the last point is the same as the first point, then this pixel is
- *				drawn only once.
- * @param	color		The color used to draw the line, alpha channel is managed.
- * @param	clipper		If not NULL, the drawing is restricted within this rectangle.
- */
 void ei_draw_polyline (ei_surface_t			surface,
 						 const ei_linked_point_t*	first_point,
 						 const ei_color_t		color,
 						 const ei_rect_t*		clipper) {
     if(first_point == NULL) return;
-    if(first_point->next == NULL) {
-        return;
-    }
-    ei_linked_point_t *ptr_tmp = (ei_linked_point_t *) first_point;
-    while(ptr_tmp->next != NULL) {
-        private_ei_draw_line(surface, ptr_tmp->point, ptr_tmp->next->point, color, clipper);
-        ptr_tmp = ptr_tmp->next;
-    }
-    private_ei_draw_line(surface, ptr_tmp->point, first_point->point, color, clipper);
+	uint8_t *buff = hw_surface_get_buffer(surface);
+	int W = hw_surface_get_size(surface).width;
+	uint32_t col = ei_map_rgba(surface, &color);
+	ei_point_t pt_min = first_point->point;
+	ei_point_t pt_max = first_point->point;
+	if(first_point->next != NULL) {
+		ei_linked_point_t *ptr_tmp = (ei_linked_point_t *) first_point;
+		while(ptr_tmp != NULL) {
+			ei_point_t pt_0 = ptr_tmp->point;
+			ei_point_t pt_1;
+			pt_1 = (ptr_tmp->next != NULL) ? ptr_tmp->next->point : first_point->point;
+			int dx = (pt_1.x > pt_0.x) ? pt_1.x - pt_0.x : pt_0.x - pt_1.x;
+			int dy = (pt_1.y > pt_0.y) ? pt_1.y - pt_0.y : pt_0.y - pt_1.y;
+			int incr_x = 1 - 2 * (pt_0.x >= pt_1.x);
+			int incr_y = 1 - 2 * (pt_0.y >= pt_1.y);
+			int E = dx - dy;
+			while(pt_0.x != pt_1.x || pt_0.y != pt_1.y) {
+				pt_min.x = (pt_min.x > pt_0.x) ? pt_min.x : pt_0.x;
+				pt_min.y = (pt_min.y > pt_0.y) ? pt_min.y : pt_0.y;
+				pt_max.x = (pt_max.x > pt_0.x) ? pt_max.x : pt_0.x;
+				pt_max.y = (pt_max.y > pt_0.y) ? pt_max.y : pt_0.y;
+				*((uint32_t*)buff + pt_0.x + W * pt_0.y) = col; //TODO clipper
+				if(2 * E > - dy) {
+					E -= dy;
+					pt_0.x += incr_x;
+				}
+				if(2 * E < dx) {
+					E += dx;
+					pt_0.y += incr_y;
+				}
+			}
+			ptr_tmp = ptr_tmp->next;
+		}
+		ei_size_t update_size = {pt_max.x - pt_min.x, pt_max.y - pt_min.y};
+		ei_rect_t rect = {pt_min, update_size};
+		ei_linked_rect_t linked_rect = {rect, NULL};
+		hw_surface_update_rects(surface, &linked_rect);
+		return;
+	}
+	if(clipper == NULL ||
+			(first_point->point.x >= clipper->top_left.x &&
+			first_point->point.y >= clipper->top_left.y &&
+			first_point->point.x <= clipper->top_left.x + clipper->size.width &&
+			first_point->point.y <= clipper->top_left.y + clipper->size.height)) {
+		*((uint32_t*)buff + first_point->point.x + W * first_point->point.y) = col;
+		ei_size_t update_size = {1, 1};
+		ei_rect_t rect = {first_point->point, update_size};
+		ei_linked_rect_t linked_rect = {rect, NULL};
+		hw_surface_update_rects(surface, &linked_rect);
+	}
 }
 void ei_draw_polygon(ei_surface_t surface,
 						 const ei_linked_point_t* first_point,
