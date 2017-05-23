f66c135f29234625d07711ebc43a0e5085232a8e
Pierre Bouvier <Pierre.Bouvier1@ensimag.grenoble-inp.fr>
2017-05-23 12:19:19 +0200

    Better ei_draw_polyline

--- src/ei_draw.c
+++ src/ei_draw.c
@@ -18,56 +18,46 @@ void ei_draw_polyline (ei_surface_t			surface,
 						 const ei_color_t		color,
 						 const ei_rect_t*		clipper) {
     if(first_point == NULL) return;
-	uint8_t *buff = hw_surface_get_buffer(surface);
+	uint32_t *buff = (uint32_t *) hw_surface_get_buffer(surface);
 	int W = hw_surface_get_size(surface).width;
 	uint32_t col = ei_map_rgba(surface, &color);
 	ei_point_t pt_min = first_point->point;
 	ei_point_t pt_max = first_point->point;
-	if(first_point->next != NULL) {
-		ei_linked_point_t *ptr_tmp = (ei_linked_point_t *) first_point;
-		while(ptr_tmp != NULL) {
-			ei_point_t pt_0 = ptr_tmp->point;
-			ei_point_t pt_1;
-			pt_1 = (ptr_tmp->next != NULL) ? ptr_tmp->next->point : first_point->point;
-			int dx = (pt_1.x > pt_0.x) ? pt_1.x - pt_0.x : pt_0.x - pt_1.x;
-			int dy = (pt_1.y > pt_0.y) ? pt_1.y - pt_0.y : pt_0.y - pt_1.y;
-			int incr_x = 1 - 2 * (pt_0.x >= pt_1.x);
-			int incr_y = 1 - 2 * (pt_0.y >= pt_1.y);
-			int E = dx - dy;
-			while(pt_0.x != pt_1.x || pt_0.y != pt_1.y) {
-				pt_min.x = (pt_min.x > pt_0.x) ? pt_min.x : pt_0.x;
-				pt_min.y = (pt_min.y > pt_0.y) ? pt_min.y : pt_0.y;
-				pt_max.x = (pt_max.x > pt_0.x) ? pt_max.x : pt_0.x;
-				pt_max.y = (pt_max.y > pt_0.y) ? pt_max.y : pt_0.y;
-				*((uint32_t*)buff + pt_0.x + W * pt_0.y) = col; //TODO clipper
-				if(2 * E > - dy) {
-					E -= dy;
-					pt_0.x += incr_x;
-				}
-				if(2 * E < dx) {
-					E += dx;
-					pt_0.y += incr_y;
-				}
+	ei_linked_point_t *ptr_tmp = (ei_linked_point_t *) first_point;
+	while(ptr_tmp != NULL) {
+		pt_min.x = (pt_min.x > ptr_tmp->point.x) ? pt_min.x : ptr_tmp->point.x;
+		pt_min.y = (pt_min.y > ptr_tmp->point.y) ? pt_min.y : ptr_tmp->point.y;
+		pt_max.x = (pt_max.x > ptr_tmp->point.x) ? pt_max.x : ptr_tmp->point.x;
+		pt_max.y = (pt_max.y > ptr_tmp->point.y) ? pt_max.y : ptr_tmp->point.y;
+		ptr_tmp = ptr_tmp->next;
+	}
+	ptr_tmp = (ei_linked_point_t *) first_point;
+	while(ptr_tmp != NULL) {
+		ei_point_t pt_0 = ptr_tmp->point;
+		ei_point_t pt_1;
+		pt_1 = (ptr_tmp->next != NULL) ? ptr_tmp->next->point : first_point->point;
+		int dx = (pt_1.x > pt_0.x) ? pt_1.x - pt_0.x : pt_0.x - pt_1.x;
+		int dy = (pt_1.y > pt_0.y) ? pt_1.y - pt_0.y : pt_0.y - pt_1.y;
+		int incr_x = 1 - 2 * (pt_0.x >= pt_1.x);
+		int incr_y = 1 - 2 * (pt_0.y >= pt_1.y);
+		int E = dx - dy;
+		do {
+			buff[pt_0.x + W * pt_0.y] = col; //TODO clipper
+			if(2 * E > - dy) {
+				E -= dy;
+				pt_0.x += incr_x;
 			}
-			ptr_tmp = ptr_tmp->next;
-		}
-		ei_size_t update_size = {pt_max.x - pt_min.x, pt_max.y - pt_min.y};
-		ei_rect_t rect = {pt_min, update_size};
-		ei_linked_rect_t linked_rect = {rect, NULL};
-		hw_surface_update_rects(surface, &linked_rect);
-		return;
+			if(2 * E < dx) {
+				E += dx;
+				pt_0.y += incr_y;
+			}
+		} while(pt_0.x != pt_1.x || pt_0.y != pt_1.y);
+		ptr_tmp = ptr_tmp->next;
 	}
-	if(clipper == NULL ||
-			(first_point->point.x >= clipper->top_left.x &&
-			first_point->point.y >= clipper->top_left.y &&
-			first_point->point.x <= clipper->top_left.x + clipper->size.width &&
-			first_point->point.y <= clipper->top_left.y + clipper->size.height)) {
-		*((uint32_t*)buff + first_point->point.x + W * first_point->point.y) = col;
-		ei_size_t update_size = {1, 1};
-		ei_rect_t rect = {first_point->point, update_size};
-		ei_linked_rect_t linked_rect = {rect, NULL};
-		hw_surface_update_rects(surface, &linked_rect);
-	}
+	ei_size_t update_size = {pt_max.x - pt_min.x, pt_max.y - pt_min.y};
+	ei_rect_t rect = {pt_min, update_size};
+	ei_linked_rect_t linked_rect = {rect, NULL};
+	hw_surface_update_rects(surface, &linked_rect);
 }
 void ei_draw_polygon(ei_surface_t surface,
 						 const ei_linked_point_t* first_point,
