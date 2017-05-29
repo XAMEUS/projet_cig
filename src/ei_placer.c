8360dec8f13ad1169dd4be53a31f4fdac2ba5cd0
Maxime <maxime.gourgoulhon@gmail.com>
2017-05-29 17:26:14 +0200

    ei_placer

--- src/ei_placer.c
+++ src/ei_placer.c
@@ -12,102 +12,73 @@ void ei_place(struct ei_widget_t* widget,
                 float* rel_y,
                 float* rel_width,
                 float* rel_height) {
-    if (widget->placer_params) { // update
-        if (anchor) {
-            widget->placer_params->anchor = anchor;
-            widget->placer_params->anchor_data = *anchor;
-        }
-        if (x) {
-            widget->placer_params->x = x;
-            widget->placer_params->x_data = *x;
-        }
-        if (y) {
-            widget->placer_params->y = y;
-            widget->placer_params->y_data = *y;
-        }
-        if (width) {
-            widget->placer_params->w = width;
-            widget->placer_params->w_data = *width;
-        }
-        if (height) {
-            widget->placer_params->h = height;
-            widget->placer_params->h_data = *height;
-        }
-        if (rel_x) {
-            widget->placer_params->rx = rel_x;
-            widget->placer_params->rx_data = *rel_x;
-        }
-        if (rel_y) {
-            widget->placer_params->ry = rel_y;
-            widget->placer_params->ry_data = *rel_y;
-        }
-        if (rel_width) {
-            widget->placer_params->rw = rel_width;
-            widget->placer_params->rw_data = *rel_width;
-        }
-        if (rel_height) {
-            widget->placer_params->rh = rel_height;
-            widget->placer_params->rh_data = *rel_height;
-        }
+    if (!(widget->placer_params)) /* init */
+        widget->placer_params = calloc(1, sizeof(ei_placer_params_t));
+    /* update */
+    if (anchor) {
+        widget->placer_params->anchor_data = *anchor;
+        widget->placer_params->anchor = &(widget->placer_params->anchor_data);
     }
-    else { // init
-        ei_placer_params_t* params = malloc(sizeof(ei_placer_params_t));
-        params->anchor = anchor;
-        params->x = x;
-        params->y = y;
-        params->w = width;
-        params->h = height;
-        params->rx = rel_x;
-        params->ry = rel_y;
-        params->rw = rel_width;
-        params->rh = rel_height;
-        if (anchor)
-            params->anchor_data = *anchor;
-        else
-            params->anchor_data = ei_anc_northwest;
-        if (x)
-            params->x_data = *x;
-        else
-            params->x_data = 0;
-        if (y)
-            params->y_data = *y;
-        else
-            params->y_data = 0;
-        if (width)
-            params->w_data = *width;
-        else if (!rel_width)
-            params->w_data = widget->requested_size.width;
-        else
-            params->w_data = 0;
-        if (height)
-            params->h_data = *height;
-        else if (!rel_height)
-            params->h_data = widget->requested_size.height;
-        else
-            params->w_data = 0;
-        if (rel_x)
-            params->rx_data = *rel_x;
-        else
-            params->rx_data = 0.0;
-        if (rel_y)
-            params->ry_data = *rel_y;
-        else
-            params->ry_data = 0.0;
-        if (rel_width)
-            params->rw_data = *rel_width;
-        else
-            params->rw_data = 0.0;
-        if (rel_height)
-            params->rh_data = *rel_height;
-        else
-            params->rh_data = 0.0;
-        widget->placer_params = params;
+    if (x) {
+        widget->placer_params->x_data = *x;
+        widget->placer_params->x = &(widget->placer_params->x_data);
+    }
+    if (y) {
+        widget->placer_params->y_data = *y;
+        widget->placer_params->y = &(widget->placer_params->y_data);
+    }
+    if (width) {
+        widget->placer_params->w_data = *width;
+        widget->placer_params->w = &(widget->placer_params->w_data);
+    }
+    if (height) {
+        widget->placer_params->h_data = *height;
+        widget->placer_params->h = &(widget->placer_params->h_data);
+    }
+    if (rel_x) {
+        widget->placer_params->rx_data = *rel_x;
+        widget->placer_params->rx = &(widget->placer_params->rx_data);
+    }
+    if (rel_y) {
+        widget->placer_params->ry_data = *rel_y;
+        widget->placer_params->ry = &(widget->placer_params->ry_data);
+    }
+    if (rel_width) {
+        widget->placer_params->rw_data = *rel_width;
+        widget->placer_params->rw = &(widget->placer_params->rw_data);
+    }
+    if (rel_height) {
+        widget->placer_params->rh_data = *rel_height;
+        widget->placer_params->rh = &(widget->placer_params->rh_data);
     }
     ei_placer_run(widget);
 }
 
 
 void ei_placer_run(struct ei_widget_t* widget) {
+
+    if (!(widget->placer_params->anchor))
+        widget->placer_params->anchor_data = ei_anc_northwest;
+    if (!(widget->placer_params->x))
+        widget->placer_params->x_data = 0;
+    if (!(widget->placer_params->y))
+        widget->placer_params->y_data = 0;
+    if (!(widget->placer_params->w) && !(widget->placer_params->rw))
+        widget->placer_params->w_data = widget->requested_size.width;
+    else if (!(widget->placer_params->w))
+        widget->placer_params->w_data = 0;
+    if (!(widget->placer_params->h) && !(widget->placer_params->rh))
+        widget->placer_params->h_data = widget->requested_size.height;
+    else if (!(widget->placer_params->h))
+        widget->placer_params->h_data = 0;
+    if (!(widget->placer_params->rx))
+        widget->placer_params->rx_data = 0.0;
+    if (!(widget->placer_params->ry))
+        widget->placer_params->ry_data = 0.0;
+    if (!(widget->placer_params->rw))
+        widget->placer_params->rw_data = 0.0;
+    if (!(widget->placer_params->rh))
+        widget->placer_params->rh_data = 0.0;
 
     ei_placer_params_t* params = widget->placer_params;
     widget->screen_location.top_left.x = widget->placer_params->x_data +
