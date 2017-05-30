4259ffb883b43c4192c783926120ad8462528c94
Maxime <maxime.gourgoulhon@gmail.com>
2017-05-30 11:16:13 +0200

    debug, show fps

--- Makefile
+++ Makefile
@@ -10,10 +10,14 @@ INCFLAGS	:= -I${INCLUDES} -I${TESTS}
 OPTFLAGS	:= -g
 CCFLAGS		:= -c ${OPTFLAGS} -Wall -std=c99
 
+DEBUG := true
+
 # The list of objects to include in the library
-
+ifeq ($(DEBUG),true)
+LIBEIOBJS	:= ./objs/freq_counter.o ./objs/ei_draw.o ./objs/ei_placer.o ./objs/ei_event.o ./objs/ei_widget.o ./objs/ei_widgetclass.o ./objs/ei_application.o ./objs/ei_frame.o ./objs/ei_button.o ./objs/ei_toplevel.o ./objs/ei_draw_polygon.o
+else
 LIBEIOBJS	:= ./objs/ei_draw.o ./objs/ei_placer.o ./objs/ei_event.o ./objs/ei_widget.o ./objs/ei_widgetclass.o ./objs/ei_application.o ./objs/ei_frame.o ./objs/ei_button.o ./objs/ei_toplevel.o ./objs/ei_draw_polygon.o
-
+endif
 
 
 # Platform specific definitions (OS X, Linux)
--- src/ei_application.c
+++ src/ei_application.c
@@ -1,6 +1,7 @@
 #include "ei_application.h"
 #include "ei_frame.h"
 #include "ei_widgetclass.h"
+#include "ei_debug.h"
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -63,33 +64,44 @@ void ei_app_free() {
  *		\ref ei_app_quit_request is called.
  */
 void ei_app_run() {
-    ei_widget_t *w = ROOT_WIDGET;
+    #ifdef DEBUG
+    frequency_counter_t* fc = malloc(sizeof(frequency_counter_t));
+    frequency_init(fc);
     while(1) {
-        if(w->placer_params)
-            w->wclass->drawfunc(w, ROOT_SURFACE, NULL, w->parent->content_rect);
-        else if(w == ROOT_WIDGET)
-            w->wclass->drawfunc(w, ROOT_SURFACE, NULL, NULL);
-        if(w->children_head != NULL)
-            w = w->children_head;
-        else if(w->next_sibling != NULL)
-            w = w->next_sibling;
-        else {
-            while(w->parent != NULL && w->parent->next_sibling == NULL)
-                w = w->parent;
-            if(w->parent != NULL)
-                w = w->parent->next_sibling;
-            else
-                break;
+    #endif
+        ei_widget_t *w = ROOT_WIDGET;
+        while(1) {
+            if(w->placer_params)
+                w->wclass->drawfunc(w, ROOT_SURFACE, NULL, w->parent->content_rect);
+            else if(w == ROOT_WIDGET)
+                w->wclass->drawfunc(w, ROOT_SURFACE, NULL, NULL);
+            if(w->children_head != NULL)
+                w = w->children_head;
+            else if(w->next_sibling != NULL)
+                w = w->next_sibling;
+            else {
+                while(w->parent != NULL && w->parent->next_sibling == NULL)
+                    w = w->parent;
+                if(w->parent != NULL)
+                    w = w->parent->next_sibling;
+                else
+                    break;
+            }
         }
+
+        struct ei_event_t* event = malloc(sizeof(struct ei_event_t*));
+        while(SHALL_WE_CONTINUE) {
+            //TODO redessin des zones 3.7
+            hw_event_wait_next(event);
+            //TODO Analyse event voir 3.6
+            sleep(5);
+            SHALL_WE_CONTINUE = EI_FALSE;
+        }
+    #ifdef DEBUG
+        frequency_tick(fc);
     }
-    struct ei_event_t* event = malloc(sizeof(struct ei_event_t*));
-    while(SHALL_WE_CONTINUE) {
-        //TODO redessin des zones 3.7
-        hw_event_wait_next(event);
-        //TODO Analyse event voir 3.6
-        sleep(5);
-        SHALL_WE_CONTINUE = EI_FALSE;
-    }
+    #endif
+
 }
 
 /**
