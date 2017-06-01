42b6db8c4c73ec1932e521f6f39b49a3143635eb
Julie Saouli <Julie.Saouli@ensimag.grenoble-inp.fr>
2017-06-01 09:43:00 +0200

    draw button & toplevel

---
+++ include/ei_draw_toplevel.h
@@ -0,0 +1,10 @@
+#ifndef EI_DRAW_TOPLEVEL_H
+#define EI_DRAW_TOPLEVEL_H
+
+void draw_toplevel(ei_surface_t surface,
+                    ei_rect_t* clipper,
+                    ei_size_t bg_size,
+                    ei_color_t bg_color,
+                    int border_width)
+
+#ifndef
