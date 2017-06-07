357b833fe34685e0c335dffc960c9e1502666001
Pierre Bouvier <Pierre.Bouvier1@ensimag.grenoble-inp.fr>
2017-06-07 10:47:36 +0200

    button doc

--- include/ei_button.h
+++ include/ei_button.h
@@ -1,5 +1,5 @@
 /**
- * @file	ei_frame.h
+ * @file	ei_button.h
  *
  * @brief 	Definition of button.
  */
@@ -10,12 +10,15 @@
 #include "ei_widget.h"
 #include "ei_frame.h"
 
+/**
+ * \brief	Fields of button. It has common fields with frame.
+ */
 typedef struct ei_button_t {
     ei_frame_t frame;
-    int corner_radius;
-    ei_callback_t callback;
-	void* user_param;
-    ei_bool_t push;
+    int corner_radius; ///< Corner radius of button.
+    ei_callback_t callback; ///< When we click on button.
+	void* user_param; ///< Pointer passed to the callback function when it's called..
+    ei_bool_t push; ///< Is the button is pushed?
 } ei_button_t;
 
 
