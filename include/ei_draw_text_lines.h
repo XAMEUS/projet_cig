c7cc5e3f035a9dca5806963417dd4f133f9258bf
Maxime <maxime.gourgoulhon@gmail.com>
2017-06-07 10:21:33 +0200

    ei_draw_text_lines doc

--- include/ei_draw_text_lines.h
+++ include/ei_draw_text_lines.h
@@ -1,3 +1,8 @@
+/**
+ *  @file	ei_draw_text_lines.h
+ *  @brief	Graphical primitives extension to draw multilines text.
+ *
+ */
 
 #ifndef EI_DRAW_TEXT_LINES_H
 #define EI_DRAW_TEXT_LINES_H
@@ -6,7 +11,19 @@
 #include "ei_types.h"
 #include "hw_interface.h"
 
-
+/**
+ * \brief	Draws text by calling \ref hw_text_create_surface. Mangages '\n' and fills the surface available with automatic new lines.
+ *
+ * @param	surface 	Where to draw the text. The surface must be *locked* by
+ *				\ref hw_surface_lock.
+ * @param	where		Coordinates, in the surface, where to anchor the top-left corner of
+ *				the rendered text.
+ * @param	text		The string of the text. Can't be NULL.
+ * @param	font		The font used to render the text. If NULL, the \ref ei_default_font
+ *				is used.
+ * @param	color		The text color. Can't be NULL. The alpha parameter is not used.
+ * @param	clipper		If not NULL, the drawing is restricted within this rectangle.
+ */
 void ei_draw_text_lines(ei_surface_t	surface,
 					const ei_point_t* where,
 					char* text,
