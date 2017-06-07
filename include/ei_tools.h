941da78a91d7982b514d25eddb14d83ea91ad69c
Maxime <maxime.gourgoulhon@gmail.com>
2017-06-07 10:15:25 +0200

    ei_tools doc

--- include/ei_tools.h
+++ include/ei_tools.h
@@ -1,3 +1,8 @@
+/**
+ *  @file	ei_tools.h
+ *  @brief	Some useful tools...
+ *
+ */
 
 #ifndef EI_UTILS_H
 #define EI_UTILS_H
@@ -5,9 +10,37 @@
 #include "ei_types.h"
 #include <stdlib.h>
 
+
+/**
+ * \brief	Calculates the intersection of two rectangles.
+ *
+ * @param	rect1     the first rectangle
+ * @param	rect2     the second rectangle
+ *
+ * @return 			the rect corresponding to the intersection (or NULL if there is no intersection).
+ */
 ei_rect_t *ei_rect_intrsct(ei_rect_t *rect1, ei_rect_t *rect2);
+
+
+/**
+ * \brief	Calculates the rectangle ('pack') containing two rectangles.
+ *
+ * @param	rect1     the first rectangle
+ * @param	rect2     the second rectangle
+ *
+ * @return 			the rect corresponding to the pack.
+ */
 ei_rect_t *ei_rect_pack(ei_rect_t *rect1, ei_rect_t *rect2);
+
+
+/**
+ * \brief	Checks if a rectangle contains a sp.
+ *
+ * @param	pt     the point
+ * @param	rect     the rectangle
+ *
+ * @return 			EI_TRUE if rect contains pt
+ */
 ei_bool_t in_rect(ei_point_t *pt, ei_rect_t *rect);
-char **strsplit(const char* str, const char* delim, size_t* numtokens);
 
 #endif
