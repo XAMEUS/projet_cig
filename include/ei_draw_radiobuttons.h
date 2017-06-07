53cd236317f3d9008a13b66a6b271e8e23a3c0b4
Julie Saouli <Julie.Saouli@ensimag.grenoble-inp.fr>
2017-06-07 13:08:27 +0200

    draw radiobutton files

---
+++ include/ei_draw_radiobuttons.h
@@ -0,0 +1,14 @@
+/**
+* @file Graphical primitive to draw radiobuttons.
+*
+* @brief Graphical primitive to draw radiobuttons.
+*/
+
+#include <stdlib.h>
+#include <assert.h>
+#include "ei_draw_radiobuttons.h"
+#include "ei_types"
+
+// ei_linked_point_t* diamond(ei_rect_t frame) {
+//
+// }
